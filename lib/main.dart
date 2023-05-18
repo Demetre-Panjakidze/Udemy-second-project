86259d89ea10f2076ef2aefbfb5896d1d73cca1e
unknown <demetrepanjakidze1@gmail.com>
2023-05-18 14:09:56 +0400

    Homepage done

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyWidget());
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "quiz app",
-      home: HomePage(),
-    );
-  }
+  runApp(const MaterialApp(
+    title: "quiz app",
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatefulWidget {
